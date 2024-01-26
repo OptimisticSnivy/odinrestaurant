153864d78f177f924cc36685c875727c19bd3106
Anand Mahamuni <m.anand7203@gmail.com>
2024-01-26 19:48:54 +0530

    Added btns

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 import _ from "lodash";
-import { mainpage } from "./mainpage.js";
+import { header, mainpage } from "./mainpage.js";
 import "./style.css";
 
+header();
 mainpage();
+
+let mainbtn = document.querySelector(".iconsChild");
+mainbtn.addEventListener("click", function (e) {
+  if ((e.innerText = "Main")) {
+    console.log("This is the mainbtn!");
+  }
+});
+
+let menubtn = document.querySelector(".iconsChild");
+menubtn.addEventListener("click", function (e) {
+  if ((e.innerText = "Menu")) {
+    console.log("This is the menubtn!");
+  }
+});
--- src/mainpage.js
+++ src/mainpage.js
@@ -1,41 +1,80 @@
 import bat from "./images/bat1.jpg";
 
-const mainpage = () => {
-	let content = document.body.querySelector(".content");
+const header = () => {
+  let content = document.body.querySelector(".content");
 
-	let header = document.createElement("div");
-	let headerText = document.createElement("div");
-	let icons = document.createElement("div");
-	let iconsMenu = document.createElement("div");
-	let iconsContact = document.createElement("div");
-	let iconsHome = document.createElement("div");
+  let header = document.createElement("div");
+  let headerText = document.createElement("div");
+  let icons = document.createElement("div");
+  let iconsMenu = document.createElement("div");
+  let iconsContact = document.createElement("div");
+  let iconsHome = document.createElement("div");
 
-	header.className = "header";
+  header.className = "header";
 
-	icons.className = "icons";
-	iconsHome.className = "iconsChild";
-	iconsMenu.className = "iconsChild";
-	iconsContact.className = "iconsChild";
+  icons.className = "icons";
+  iconsHome.className = "iconsChild";
+  iconsMenu.className = "iconsChild";
+  iconsContact.className = "iconsChild";
 
-	iconsHome.innerText = "Home";
-	iconsMenu.innerText = "Menu";
-	iconsContact.innerText = "Contact";
+  iconsHome.innerText = "Home";
+  iconsHome.id = "Home";
+  iconsMenu.innerText = "Menu";
+  iconsMenu.id = "Menu";
+  iconsContact.innerText = "Contact";
+  iconsContact.id = "Contact";
 
-	headerText.innerText = "GOTHAM CITY CAFE";
-	headerText.className = "headerText";
+  headerText.innerText = "GOTHAM CITY CAFE";
+  headerText.className = "headerText";
 
-	const batIcon = new Image();
-	batIcon.src = bat;
+  const batIcon = new Image();
+  batIcon.src = bat;
 
-	icons.appendChild(iconsHome);
-	icons.appendChild(iconsMenu);
-	icons.appendChild(iconsContact);
+  icons.appendChild(iconsHome);
+  icons.appendChild(iconsMenu);
+  icons.appendChild(iconsContact);
 
-	header.appendChild(headerText);
-	header.appendChild(batIcon);
+  header.appendChild(headerText);
+  header.appendChild(batIcon);
 
-	content.appendChild(header);
-	content.appendChild(icons);
+  content.appendChild(header);
+  content.appendChild(icons);
+
+  let menubtn = document.querySelector("#Menu");
+  menubtn.addEventListener("click", function (e) {
+    console.log("This is the menubtn!");
+  });
+
+  let contactbtn = document.querySelector("#Contact");
+  contactbtn.addEventListener("click", function (e) {
+    console.log("This is the contactbtn!");
+  });
+
+  let mainbtn = document.querySelector("#Home");
+  mainbtn.addEventListener("click", function (e) {
+    console.log("This is the mainbtn!");
+  });
 };
 
-export { mainpage };
+
+const mainpage = () => {
+  let content = document.body.querySelector(".content");
+
+  let main = document.createElement("div");
+  main.innerText = "This is the main!";
+  main.className = "mainpage";
+
+  content.appendChild(main);
+};
+
+const menupage = () => {
+  let content = document.body.querySelector(".content");
+
+  let menu = document.createElement("div");
+  menu.innerText = "This is the menu!";
+  menu.className = "content";
+
+  content.appendChild(menu);
+};
+
+export { header, mainpage, menupage };
