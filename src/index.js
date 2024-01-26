dcea001688437f56001ed7452da5dbd058ed0c13
Anand Mahamuni <m.anand7203@gmail.com>
2024-01-26 20:08:47 +0530

    removed useless eventlisteners!

--- src/index.js
+++ src/index.js
@@ -3,18 +3,3 @@ import { header, mainpage } from "./mainpage.js";
 import "./style.css";
 
 header();
-mainpage();
-
-let mainbtn = document.querySelector(".iconsChild");
-mainbtn.addEventListener("click", function (e) {
-  if ((e.innerText = "Main")) {
-    console.log("This is the mainbtn!");
-  }
-});
-
-let menubtn = document.querySelector(".iconsChild");
-menubtn.addEventListener("click", function (e) {
-  if ((e.innerText = "Menu")) {
-    console.log("This is the menubtn!");
-  }
-});
